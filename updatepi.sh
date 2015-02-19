33c31ca6ce07a5b59e12e3b890f98ba1314752d9
Loral <loral_godfrey@yahoo.com>
2015-02-18 22:55:53 -0700

    Update updatepi.sh

--- updatepi.sh
+++ updatepi.sh
@@ -3,10 +3,10 @@ date
 echo UPDATING SYSTEM SOFTWARE . UPDATE
 sudo apt-get update
 echo UPDATING SYSTEM SOFTWARE . UPGRADE
-sudo apt-get upgrade
-echo UPDATING SYSTEM SOFTWARE . DISTRIBUTION
-sudo apt-get dist-upgrade
+sudo apt-get upgrade -y
+#echo UPDATING SYSTEM SOFTWARE . DISTRIBUTION
+#sudo apt-get dist-upgrade
 echo REMOVING APPLICATION ORPHANS
-sudo apt-get autoremove .purge
-echo UPDATING FIRMWARE
-sudo rpi-update
+sudo apt-get autoremove -y
+#echo UPDATING FIRMWARE
+#sudo rpi-update
